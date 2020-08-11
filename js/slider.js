47f1a7c5a2b553d74e8282bd59e2ebcf706d1d8c
ilg2005 <igoldfarb10@gmail.com>
2020-08-11 23:33:48 +0300

    карусель работает

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,21 @@
-'use strict';
+$(document).ready(function(){
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5500,
+    autoplaySpeed: 3000,
+    autoplayHoverPause: true,
+    slideTransition: '',
+    items: 1,
+    nav: false,
+    dots: true,
 
-var isMouseInSlider = false;
-var sliderListElement = $('.slider-list');
-
-sliderListElement.mouseenter(function () {
-  isMouseInSlider = true;
-  console.log(isMouseInSlider);
-});
-
-var slideIDs = $('input[type="radio"]').map(function () {
-  return this.id;
-}).get();
-
-function clickSlide(slideId) {
-  $('#' + slideId).trigger('click');
-}
-
-slideIDs.forEach(function (id, i) {
-  setTimeout(function () {
-    clickSlide(id);
-  }, 1000 * ++i)
+    responsive: {
+      600: {
+        items: 1
+      }
+    }
+  });
 });
 
